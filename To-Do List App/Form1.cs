df4345c47265f842d1f740debadd5d6ba3128cdb
potuta <cyrilbautista2@gmail.com>
2024-04-19 18:49:36 +0800

    Changes assigned.

--- To-Do List App/Form1.cs
+++ To-Do List App/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Deployment.Application;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,7 @@ namespace To_Do_List_App
         private void TDL_Form_Load(object sender, EventArgs e)
         {
             MaximizeBox = false;
+            checkUpdates();
         }
 
         private void TDL_notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
@@ -94,6 +96,39 @@ namespace To_Do_List_App
                 addToListButton.Enabled = true;
                 textBox.Enabled = true;
                 textBox.Clear();
+            }
+        }
+
+        private void checkUpdates()
+        {
+            UpdateCheckInfo info;
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+                try
+                {
+                    info = ad.CheckForDetailedUpdate();
+                }
+                catch (DeploymentDownloadException dde)
+                {
+                    MessageBox.Show("The new version of the application can't be downloaded at this time.\n\nPlease check your network connection or try again later. Error: " + dde.Message, "Message", MessageBoxButtons.OK);
+                    return;
+                }
+                if (info.UpdateAvailable)
+                {
+                    if (MessageBox.Show("A newer version of the application is available. Would you like to update it now? ", "Message" ,MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        try
+                        {
+                            ad.Update();
+                            Application.Restart();
+                        }
+                        catch(Exception e)
+                        {
+                            MessageBox.Show(e.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
             }
         }
     }
