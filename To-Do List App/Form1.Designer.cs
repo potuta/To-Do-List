8306fd979e309fb396a1b2cb01e35329d71c9602
potuta <cyrilbautista2@gmail.com>
2024-04-19 21:41:42 +0800

    Changes assigned.

--- To-Do List App/Form1.Designer.cs
+++ To-Do List App/Form1.Designer.cs
@@ -82,9 +82,9 @@ namespace To_Do_List_App
             this.label1.Font = new System.Drawing.Font("Segoe Print", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(97, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(266, 47);
+            this.label1.Size = new System.Drawing.Size(166, 47);
             this.label1.TabIndex = 1;
-            this.label1.Text = "I love you bebe <3";
+            this.label1.Text = "To-Do List";
             // 
             // label2
             // 
